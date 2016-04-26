5c5b9159b6fd845be3e426a23b9bc8081baab545
Markcial <markcial@gmail.com>
2016-04-26 06:46:18 +0200

    Added default flag for lesser interactive commands, tests included

--- test/get.fish
+++ test/get.fish
@@ -17,3 +17,33 @@ test "$TESTNAME - Use --rule=regex to validate user input"
         echo 123 | get --rule="[0-9]"
         )
 end
+
+test "$TESTNAME - Use -d=value flag"
+    y = (
+        echo | get -d y
+        )
+end
+
+test "$TESTNAME - Use --default=value flag"
+    y = (
+        echo | get --default y
+        )
+end
+
+test "$TESTNAME - Use --default=value flag with --regex"
+    y = (
+        echo | get --default y --rule="^[yn]\$"
+        )
+end
+
+test "$TESTNAME - Use --default=value flag with --silent"
+    y = (
+        echo | get --default y --silent
+        )
+end
+
+test "$TESTNAME - Use --default=value flag with --no-cursor"
+    y = (
+        echo | get --default y --no-cursor
+        )
+end
